b5261a53742e170a28859483594d5317ef2124f9
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2018-11-22 14:31:01 -0500

    products page done

--- LOG4420_TP5/client/src/app/products/products.component.ts
+++ LOG4420_TP5/client/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../products.service'
 import { Product } from '../products.service'
+import { ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+
 /**
  * Defines the component responsible to manage the display of the products page.
  */
@@ -9,8 +11,27 @@ import { Product } from '../products.service'
   templateUrl: './products.component.html'
 })
 export class ProductsComponent {
+  @ViewChild('camerasButton') camerasButton: ElementRef;
+  @ViewChild('consolesButton') consolesButton: ElementRef;
+  @ViewChild('screensButton') screensButton: ElementRef;
+  @ViewChild('computersButton') computersButton: ElementRef;
+  @ViewChild('allButton') allButton: ElementRef;
+
+  @ViewChild('priceAscButton') priceAscButton: ElementRef;
+  @ViewChild('priceDscButton') priceDscButton: ElementRef;
+  @ViewChild('alphaAscButton') alphaAscButton: ElementRef;
+  @ViewChild('alphaDscButton') alphaDscButton: ElementRef;
+
+  categoryButtons: ElementRef[]; 
+  criteriaButtons: ElementRef[];
+
   products: Product[];
   classifierState: ClassifierState;
+  
+  ngAfterViewInit() {
+    this.categoryButtons = [this.camerasButton, this.consolesButton, this.screensButton, this.computersButton, this.allButton];
+    this.criteriaButtons = [this.priceAscButton, this.priceDscButton, this.alphaAscButton, this.alphaDscButton];
+  }
 
   constructor(private productsService: ProductsService) {
     this.classifierState = {
@@ -20,10 +41,6 @@ export class ProductsComponent {
     this.getProducts();
   }
 
-  logProducts() {
-    console.log(this.products);
-  }
-
   getProducts(): void {
     this.productsService.getProducts(this.classifierState.criteria, this.classifierState.category).then((products) => {
       this.products = products;
@@ -31,48 +48,78 @@ export class ProductsComponent {
   }
 
   getCameras() {
+    this.unselectCategories();
+    this.camerasButton.nativeElement.classList.add("selected");
     this.classifierState.category = Category.cameras;
     this.getProducts();
   }
 
   getConsoles() {
+    this.unselectCategories();
+    this.consolesButton.nativeElement.classList.add("selected");
     this.classifierState.category = Category.consoles;
     this.getProducts();
   }
 
   getScreens() {
+    this.unselectCategories();
+    this.screensButton.nativeElement.classList.add("selected");
     this.classifierState.category = Category.screens;
     this.getProducts();
   }
 
   getComputers() {
+    this.unselectCategories();
+    this.computersButton.nativeElement.classList.add("selected");
     this.classifierState.category = Category.computers;
     this.getProducts();
   }
 
   getAll() {
+    this.unselectCategories();
+    this.allButton.nativeElement.classList.add("selected");
     this.classifierState.category = Category.all;
     this.getProducts();
   }
 
   getPriceAsc() {
+    this.unselectCriterias();
+    this.priceAscButton.nativeElement.classList.add("selected");
     this.classifierState.criteria = Criteria.priceAsc;
     this.getProducts();
   }
 
   getPriceDsc() {
+    this.unselectCriterias();
+    this.priceDscButton.nativeElement.classList.add("selected");
     this.classifierState.criteria = Criteria.priceDsc;
     this.getProducts();
   }
 
   getAlphaAsc() {
+    this.unselectCriterias();
+    this.alphaAscButton.nativeElement.classList.add("selected");
     this.classifierState.criteria = Criteria.alphaAsc;
     this.getProducts();
   }
   
   getAlphaDsc() {
+    this.unselectCriterias();
+    this.alphaDscButton.nativeElement.classList.add("selected");
     this.classifierState.criteria = Criteria.alphaDsc;
     this.getProducts();
+  }
+
+  unselectCategories() {
+    for (var i = 0; i < this.categoryButtons.length; i++) {
+      this.categoryButtons[i].nativeElement.classList.remove("selected");
+    }
+  }
+
+  unselectCriterias() {
+    for (var i = 0; i < this.criteriaButtons.length; i++) {
+      this.criteriaButtons[i].nativeElement.classList.remove("selected");
+    }
   }
 }
 
